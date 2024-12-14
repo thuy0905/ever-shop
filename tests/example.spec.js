dcfd8b1d73fe1f8d4dd72acc220f3f37d70448fc
Nguyen Thuy <anhquan0905@gmail.com>
2024-12-14 20:48:26 +0700

    step commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,20 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
+const allure = require('allure-js-commons');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+async function inputTextByLable(label,value,page){
+  let inputXpath =`(//label[./text()[normalize-space()='${label}']]/following::input)[1]`;
+  await page.locator(inputXpath).fill(value);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+}
+
+test('Verify admin login successfully', async ({ page }) => {
+  await page.goto("http://localhost:3000/admin/login");
+  await expect(page).toHaveTitle("Admin Login");
+  await inputTextByLable("Email","test@with.me",page);
+  await inputTextByLable("Password","123456",page);
+  let signInButtonXpath= "//button[.//text()[normalize-space()='SIGN IN']]";
+  await page.locator(signInButtonXpath).click();
+  let logoTextXpath="//div[contains(concat(' ',@class,' '),' logo ') and .//text()[normalize-space()='EVERSHOP']]";
+  await expect(page.locator(logoTextXpath)).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
